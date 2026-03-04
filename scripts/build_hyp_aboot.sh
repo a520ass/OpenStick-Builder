b2d5467a3ca1131274a9e30a409370b33d7a470b
hefeng <hffhhffh3@gmail.com>
2026-03-04 09:06:25 +0800

    Update build_hyp_aboot.sh

--- scripts/build_hyp_aboot.sh
+++ scripts/build_hyp_aboot.sh
@@ -7,7 +7,7 @@ make -j$(nproc) -C src/qhypstub CROSS_COMPILE=aarch64-linux-gnu-
 echo 'DEFINES += USE_TARGET_HS200_CAPS=1' >> src/lk2nd/project/lk1st-msm8916.mk
 
 
-make -j$(nproc) -C src/lk2nd LK2ND_BUNDLE_DTB="msm8916-512mb-mtp.dtb" LK2ND_COMPATIBLE="yiming,uz801-v3" \
+make -j$(nproc) -C src/lk2nd LK2ND_BUNDLE_DTB="msm8916-512mb-mtp.dtb" LK2ND_COMPATIBLE="thwc,ufi001c" \
     TOOLCHAIN_PREFIX=arm-none-eabi- lk1st-msm8916
 
 # test sign
