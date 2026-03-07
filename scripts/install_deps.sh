5b26e5a61a250603d0437b9edc1c4a7186daf0b7
a520a <a520ass@hotmail.com>
2026-03-07 21:11:58 +0800

    up5

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -24,11 +24,3 @@ apt install -y \
     qemu-user-static \
     unzip \
     wget 
-
-# 导入 Debian Bookworm 主密钥
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6ED0E7B82643E131
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 78DBA3BC47EF2265
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F8D2585B8783D481
-
-# 安装 Debian 密钥环包（确保密钥文件完整）
-sudo apt update && sudo apt install -y debian-archive-keyring
