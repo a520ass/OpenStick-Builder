35d52a004320271fcd03e4658adffdfe797f78b4
a520a <a520ass@hotmail.com>
2026-03-30 15:04:22 +0800

    up

--- scripts/debootstrap.sh
+++ scripts/debootstrap.sh
@@ -95,7 +95,7 @@ chmod +x ${CHROOT}/usr/sbin/wifi-ap.sh
 cp -a scripts/msm-firmware-loader.sh ${CHROOT}/usr/sbin
 
 # install kernel
-wget -O - http://mirror.postmarketos.org/postmarketos/master/aarch64/linux-postmarketos-qcom-msm8916-6.12.1-r2.apk \
+wget -O - http://mirror.postmarketos.org/postmarketos/v25.12/aarch64/linux-postmarketos-qcom-msm8916-6.12.1-r2.apk \
     | tar xkzf - -C ${CHROOT} --exclude=.PKGINFO --exclude=.SIGN* 2>/dev/null
 
 mkdir -p ${CHROOT}/boot/extlinux
